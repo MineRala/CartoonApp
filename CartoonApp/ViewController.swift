b36937174dd45e3c449d8c4106097cedc227d479
Mine Rala <mine.rala@rapsodo.com>
2023-03-01 12:49:00 +0300

    Initial Commit

---
+++ CartoonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CartoonApp
+//
+//  Created by Mine Rala on 1.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
